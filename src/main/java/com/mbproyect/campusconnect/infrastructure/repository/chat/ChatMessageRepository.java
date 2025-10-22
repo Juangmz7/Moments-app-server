78652ed5ac75273ff6b3e77e8f6805128eaf1f1a
juan <juanjuangmo@gmail.com>
2025-10-22 21:16:28 +0200

    Added new functionalities to the repo

--- src/main/java/com/mbproyect/campusconnect/infrastructure/repository/chat/ChatMessageRepository.java
+++ src/main/java/com/mbproyect/campusconnect/infrastructure/repository/chat/ChatMessageRepository.java
@@ -1,4 +1,13 @@
 package com.mbproyect.campusconnect.infrastructure.repository.chat;
 
-public class ChatMessageRepository {
+import com.mbproyect.campusconnect.dto.chat.response.ChatMessageResponse;
+import com.mbproyect.campusconnect.model.entity.chat.ChatMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, UUID> {
+    ChatMessageResponse findChatMessageById(UUID id);
 }
--- src/main/java/com/mbproyect/campusconnect/infrastructure/repository/user/UserRepository.java
+++ src/main/java/com/mbproyect/campusconnect/infrastructure/repository/user/UserRepository.java
@@ -1,6 +1,7 @@
 package com.mbproyect.campusconnect.infrastructure.repository.user;
 
 import com.mbproyect.campusconnect.model.entity.user.User;
+import com.mbproyect.campusconnect.model.entity.user.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    UserProfile findUserByUserProfile_Id(UUID userProfileId);
 }
