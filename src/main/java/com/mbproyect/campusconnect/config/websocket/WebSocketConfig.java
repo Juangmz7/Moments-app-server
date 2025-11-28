b5cf444c549c0f369bbdd0f97354ace30498d585
Juangmz7 <juanjuangmo@gmail.com>
2025-11-28 13:28:32 +0100

    Disabled SockJs from chat message websocket

--- src/main/java/com/mbproyect/campusconnect/config/websocket/WebSocketConfig.java
+++ src/main/java/com/mbproyect/campusconnect/config/websocket/WebSocketConfig.java
@@ -13,8 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat") // Initial endpoint for websocket connection
-                .setAllowedOriginPatterns("*")  // allow all origins
-                .withSockJS();
+                .setAllowedOriginPatterns("*");  // allow all origins
     }
 
     @Override
